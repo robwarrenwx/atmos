37e2303998de336321d22dfe3b12e9f3711d6eb1
raw565 <rob.warren1@bom.gov.au>
2023-09-13 15:42:03 +1000

    Revised constants

--- atmos/constant.py
+++ atmos/constant.py
@@ -1,54 +1,56 @@
 """
 References:
 
-Wagner, W., and A. Pruß, 2002: The IAPWS Formulation 1995 for the 
-    Thermodynamic Properties of Ordinary Water Substance for General and
-    Scientific Use. J. Phys. Chem. Ref. Data, 31, 387-535.
+Ambaum, M. H., 2020. Accurate, simple equation for saturated vapour pressure
+    over water and ice. Quart. J. Roy. Met. Soc., 146, 4252-4258.
 
 Feistel, R., and W. Wagner, 2006: A New Equation of State for H2O Ice Ih.
     J. Phys. Chem. Ref. Data, 35, 2021-1047.
 
-Ambaum, M. H., 2020. Accurate, simple equation for saturated vapour
-    pressure over water and ice. Quart. J. Roy. Met. Soc., 146, 4252-4258.
+Guildner, L. A., D. P. Johnson, and F. E. Jones, 1976: Vapor pressure of water
+    at its triple point.J. Res. Natl. Bur. Stand., 80A, 505-521.
+
+Wagner, W., and A. Pruß, 2002: The IAPWS Formulation 1995 for the 
+    Thermodynamic Properties of Ordinary Water Substance for General and
+    Scientific Use. J. Phys. Chem. Ref. Data, 31, 387-535.
 
 """
 
-
-# Acceleration due to gravity (m2/s2)
-g = 9.80665      
+# Acceleration due to gravity (m/s2)
+g = 9.81
 
 # Specific gas constant for dry air (J/kg/K)
-Rd = 287.04
+Rd = 287.0
 
 # Specific gas constant for water vapour (J/kg/K)
-Rv = 461.52  # Wagner and Pruß (2002)
+Rv = 461.5
 
 # Ratio of gas constants
 eps = Rd / Rv
 
 # Isobaric specific heat of dry air (J/kg/K)
-cpd = 1005.7
+cpd = 1005.0
 
 # Isobaric specific heat of water vapour (J/kg/K)
-cpv = 1884.4  # Wagner and Pruß (2002)
+cpv = 2040.0  # Optimised value from Ambaum (2020)
 
 # Isobaric specific heat of liquid water (J/kg/K)
-cpl = 4219.9  # Wagner and Pruß (2002)
+cpl = 4220.0  # triple-point value from Wagner and Pruß (2002)
 
 # Isobaric specific heat of ice (J/kg/K)
-cpi = 2096.8  # Feistel and Wagner (2006)
+cpi = 2097.0  # triple-point value from Feistel and Wagner (2006)
 
 # Triple point temperature (K)
 T0 = 273.16
 
 # Saturation vapour pressure at the triple point (Pa)
-es0 = 611.655  # Wagner and Pruß (2002)
+es0 = 611.657  # Guildner et al. (1976)
 
 # latent heat of vaporisation at the triple point (J/kg)
-Lv0 = 2.501e6  # Ambaum (2020)
+Lv0 = 2.501e6  # Wagner and Pruß (2002)
 
 # latent heat of freezing at the triple point (J/kg)
-Lf0 = 3.334e5  # Ambaum (2020)
+Lf0 = 0.333e6  # Feistel and Wagner (2006)
 
 # Latent heat of sublimation at the triple point (J/kg)
 Ls0 = Lv0 + Lf0  
